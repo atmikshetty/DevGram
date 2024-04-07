2872e3f5d4cf4d468e7af76b1ea992e48e7c95e0
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-04-07 10:35:07 +0530

    Sign up Page Done

--- devgram/src/_auth/forms/SignupForm.tsx
+++ devgram/src/_auth/forms/SignupForm.tsx
@@ -1,11 +1,129 @@
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignUpValidation } from "@/lib/validation";
+import { z } from "zod";
+import { ButtonLoading } from "@/components/common/ButtonLoading";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
-  return (
-    <div>
-      <Button>Sign up</Button>
-    </div>
-  )
-}
 
-export default SignupForm
+  const isLoading = false;
+
+  // Check the Shadcn Form and Zod Documentation for this
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignUpValidation>>({
+    resolver: zodResolver(SignUpValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignUpValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo-white.svg" alt="logo" className="w-36" />
+
+        <h2 className="h3-bold md:h2-bold sm:pt-3">
+          Create a new Account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">Please enter your details</p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {
+              isLoading ? (
+                <div className="flex-center gap-2">
+                  <ButtonLoading />
+                </div>
+              ) : 'Sign up'
+            }
+          </Button>
+
+            {/* Sign in Details */}
+
+            <p className="text-small-regular text-light-2 text-center mt-1">
+              Already have an account?
+              <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Login</Link>
+            </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SignupForm;
---
+++ devgram/src/lib/validation/index.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const SignUpValidation = z.object({
+    name: z.string().min(3,{
+        message: "Name must be atleast 3 characters."
+    }),
+    username: z.string().min(3, {
+      message: "Username must be at least 3 characters.",
+    }),
+    email: z.string().email(),
+    password: z.string().min(8,{
+        message: "Password must be atleast 8 characters."
+    })
+  });
